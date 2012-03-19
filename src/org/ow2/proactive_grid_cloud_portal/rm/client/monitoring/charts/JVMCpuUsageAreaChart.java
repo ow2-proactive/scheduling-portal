813ce6963de93680a17c09215b1cf2847159a35c
oleg_smirnov <oleg_smirnov@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2012-03-19 09:44:21 +0000

    PORTAL-206 Cpu usage corrected in the node monitoring
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/proactive_grid_cloud_portal/web_ui/scheduler_rm/trunk@21597 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/ow2/proactive_grid_cloud_portal/rm/client/monitoring/charts/JVMCpuUsageAreaChart.java
+++ src/org/ow2/proactive_grid_cloud_portal/rm/client/monitoring/charts/JVMCpuUsageAreaChart.java
@@ -36,8 +36,14 @@
  */
 package org.ow2.proactive_grid_cloud_portal.rm.client.monitoring.charts;
 
+import java.util.Date;
+
 import org.ow2.proactive_grid_cloud_portal.rm.client.RMController;
 
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.i18n.client.DateTimeFormat.PredefinedFormat;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.visualization.client.visualizations.corechart.AxisOptions;
 
 /**
@@ -45,11 +51,40 @@ import com.google.gwt.visualization.client.visualizations.corechart.AxisOptions;
  */
 public class JVMCpuUsageAreaChart extends MBeanTimeAreaChart {
 	
+	long prevCpu = 0;
+	long prevTime = 0;
+	
 	public JVMCpuUsageAreaChart(RMController controller, String jmxServerUrl) {
-		super(controller, jmxServerUrl, "java.lang:type=OperatingSystem", "SystemLoadAverage", "Cpu Usage");
+		super(controller, jmxServerUrl, "java.lang:type=OperatingSystem", "ProcessCpuTime", "Cpu Usage");
 		
 		AxisOptions vAxis = AxisOptions.create();
 		vAxis.set("format", "#%");
 		loadOpts.setVAxisOptions(vAxis);
 	}
+	
+	@Override
+	public void processResult(String result) {
+		JSONArray array = JSONParser.parseStrict(result).isArray();
+		if (array != null) {
+			long curTime = System.currentTimeMillis();
+			long curCpu = (long)array.get(0).isObject().get("value").isNumber().doubleValue();
+
+			if (prevTime > 0) {
+				addRow();
+
+				String formattedTime = DateTimeFormat.getFormat(PredefinedFormat.HOUR24_MINUTE).format(new Date(curTime));
+				double cpuPercent = (curCpu - prevCpu)/(10000*(curTime - prevTime));
+				// as it will be formatted to percents divide by 100
+				cpuPercent /= 100;
+				loadTable.setValue(loadTable.getNumberOfRows()-1, 0, formattedTime);				
+				loadTable.setValue(loadTable.getNumberOfRows()-1, 1, cpuPercent);
+			}
+			
+			prevCpu = curCpu;
+			prevTime = curTime;
+			
+			loadChart.draw(loadTable, loadOpts);
+		}
+	}
+	
 }
