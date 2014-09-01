e5a49d2add6166bd7660e554eb024aaf797c1d59
youribonnaffe <youri.bonnaffe@activeeon.com>
2014-09-01 08:56:15 +0200

    Update copyright year in about dialog
    Change-Id: I5c9af90089cc982b34575100fee2e212c95083ec

--- common-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/common/shared/Config.java
+++ common-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/common/shared/Config.java
@@ -169,7 +169,7 @@ public abstract class Config {
     private static final String d_ABOUT = "<h3>ProActive @application_name@ Portal</h3>" +
             "Version: @version@" +
             "<br><br>" +
-            "Copyright (C) 1997-2013 INRIA/University of Nice-Sophia Antipolis/ActiveEon<br><br>" +
+            "Copyright (C) 1997-2014 INRIA/University of Nice-Sophia Antipolis/ActiveEon<br><br>" +
             "Visit <a target='_blank' href='http://proactive.inria.fr/'>http://proactive.inria.fr/</a> " +
             "and <a target='_blank' href='http://www.activeeon.com/'>http://www.activeeon.com/</a><br>" +
             "Contact: +33 (0)9 88 777 660, <a target='_blank' href='mailto:contact@activeeon.com'>contact@activeeon.com</a>" +
