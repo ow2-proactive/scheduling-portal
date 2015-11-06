9cfe17444fb2aa3fea1f2764ca6433c47bbef111
sandrineBeauche <sandrine.beauche@activeeon.com>
2015-11-06 17:34:00 +0100

    set default display mode

--- scheduler-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/scheduler/client/model/ExecutionsModel.java
+++ scheduler-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/scheduler/client/model/ExecutionsModel.java
@@ -8,7 +8,7 @@ import org.ow2.proactive_grid_cloud_portal.scheduler.client.controller.Execution
 
 public class ExecutionsModel {
 
-    protected ExecutionListMode mode;
+    protected ExecutionListMode mode = ExecutionListMode.JOB_CENTRIC;
     
     protected ArrayList<ExecutionDisplayModeListener> modeListeners;
     
