25c9c89e883881554584e5106813f4dd66dd1bca
sandrineBeauche <sandrine.beauche@activeeon.com>
2015-11-13 12:22:57 +0100

    remove visu column in task-centric

--- scheduler-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/scheduler/client/view/grid/tasks/TasksCentricColumnsFactory.java
+++ scheduler-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/scheduler/client/view/grid/tasks/TasksCentricColumnsFactory.java
@@ -54,7 +54,7 @@ public class TasksCentricColumnsFactory extends TasksColumnsFactory{
     public GridColumns[] getColumns() {
         return new GridColumns[]{ID_ATTR, STATUS_ATTR, NAME_ATTR, TAG_ATTR, JOB_ID_ATTR, JOB_NAME_ATTR, EXEC_DURATION_ATTR, NODE_COUNT_ATTR, 
                 EXECUTIONS_ATTR, NODE_FAILURE_ATTR, HOST_ATTR, START_TIME_ATTR, FINISHED_TIME_ATTR,
-                DESCRIPTION_ATTR, VISU_ATTR};
+                DESCRIPTION_ATTR};
     }
 
     @Override
