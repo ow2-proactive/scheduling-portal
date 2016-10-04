91549087f73eaa0e1c40065f3b6bf7c6883e723c
Yinan Liu <liu.damian@gmail.com>
2016-10-04 22:58:26 +0200

    add private url generation in the calendar popup window

--- scheduler-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/scheduler/client/CalendarInfoWindow.java
+++ scheduler-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/scheduler/client/CalendarInfoWindow.java
@@ -38,6 +38,13 @@ import org.ow2.proactive_grid_cloud_portal.common.client.Images;
 import org.ow2.proactive_grid_cloud_portal.common.client.ImagesUnbundled;
 import org.ow2.proactive_grid_cloud_portal.common.client.model.LoginModel;
 import org.ow2.proactive_grid_cloud_portal.common.shared.Config;
+import org.ow2.proactive_grid_cloud_portal.scheduler.shared.SchedulerConfig;
+
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
 import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.widgets.HTMLPane;
 import com.smartgwt.client.widgets.IButton;
@@ -48,9 +55,28 @@ import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.VLayout;
 
+
 public class CalendarInfoWindow {
 
-    private Window window = null;
+    private final Window window = new Window();
+
+    private final HLayout buttons = new HLayout();
+
+    private final HLayout pane = new HLayout();
+
+    private final IButton ok = new IButton("Close");
+
+    private final IButton regenerateBt = new IButton("Regenerate");
+
+    private final IButton deleteBt = new IButton("Delete");
+
+    private final IButton createBt = new IButton("Create URL");
+
+    private final Img img = new Img(ImagesUnbundled.ABOUT_115, 115, 130);
+
+    private final VLayout root = new VLayout();
+
+    private final HTMLPane text = new HTMLPane();
 
     /**
      * Default constructor
@@ -60,55 +86,64 @@ public class CalendarInfoWindow {
     }
 
     private void build() {
-        HLayout pane = new HLayout();
         pane.setWidth100();
-        pane.setHeight(240);
+        pane.setHeight(350);
         pane.setBackgroundColor("#ffffff");
 
-        HTMLPane text = new HTMLPane();
-        text.setContents(createContext());
-        text.setWidth100();
-        text.setStyleName("paddingLeftAndRight");
-
-
-        Img img = new Img(ImagesUnbundled.ABOUT_115, 115, 130);
-
-        pane.addMember(img);
-        pane.addMember(text);
-
-        HLayout buttons = new HLayout();
         buttons.setAlign(Alignment.RIGHT);
         buttons.setWidth100();
         buttons.setHeight(20);
         buttons.setMargin(10);
-        IButton ok = new IButton("Close");
+
+        text.setWidth100();
+        text.setStyleName("paddingLeftAndRight");
+
+        root.setBackgroundColor("#dddddd");
+        root.setWidth100();
+        root.setHeight100();
+
+        window.setTitle("Calendar Integration");
+        window.setShowMinimizeButton(false);
+        window.setShowShadow(true);
+        window.setIsModal(true);
+        window.setShowModalMask(true);
+        window.setWidth(729);
+        window.setHeight(425);
+        window.centerInPage();
+        window.setCanDragReposition(false);
+
         ok.setIcon(Images.instance.cancel_16().getSafeUri().asString());
         ok.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
                 CalendarInfoWindow.this.hide();
             }
         });
+
         buttons.addMember(ok);
 
-        VLayout root = new VLayout();
-        root.setBackgroundColor("#dddddd");
-        root.setWidth100();
-        root.setHeight100();
+        regenerateBt.setIcon(Images.instance.ok_16().getSafeUri().asString());
+        regenerateBt.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                loadWindowsContent(RequestBuilder.PUT);
+            }
+        });
 
-        root.addMember(pane);
-        root.addMember(buttons);
+        deleteBt.setIcon(Images.instance.clear_16().getSafeUri().asString());
+        deleteBt.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                loadWindowsContent(RequestBuilder.DELETE);
+            }
+        });
 
-        this.window = new Window();
-        this.window.setTitle("Calendar integration");
-        this.window.setShowMinimizeButton(false);
-        this.window.setShowShadow(true);
-        this.window.setIsModal(true);
-        this.window.setShowModalMask(true);
-        this.window.addItem(root);
-        this.window.setWidth(600);
-        this.window.setHeight(319);
-        this.window.centerInPage();
-        this.window.setCanDragReposition(false);
+        createBt.setIcon(Images.instance.ok_16().getSafeUri().asString());
+        createBt.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                loadWindowsContent(RequestBuilder.POST);
+            }
+        });
+
+        loadWindowsContent(RequestBuilder.GET);
+
     }
 
     /**
@@ -133,28 +168,107 @@ public class CalendarInfoWindow {
         this.window.destroy();
     }
 
-    private String createContext(){
-        return fillTemplate();
+    private final static String CALENDAR_CONTEXT = "<h1>ProActive Scheduling & Orchestration: integration with Calendars </h1>" +
+        "<font size=\"3\">Secured Calendar URL with authentication (Apple Calendaar, Thunderbird): <br><br><i>" +
+        "http://" + com.google.gwt.user.client.Window.Location.getHostName() + ":5232/" +
+        LoginModel.getInstance().getLogin() + "/calendar.ics/</i></font><br><br>";
+
+    private String getDocumentVersion() {
+        return Config.get().getVersion().contains("SNAPSHOT") ? "latest" : Config.get().getVersion();
     }
 
-    private final static String CALENDAR_CONTEXT =
-            "<h1>ProActive Scheduling & Orchestration: integration with Calendars </h1>" +
-            "<font size=\"3\">"+
-            "Import this URL in your favorite Calendar (Outlook, Google, Thunderbird, etc.): <br><br>" +
-            "http://" + "@hostURL@" + ":5232/" + "@login@"  + "/calendar.ics/"+
-            "<br><br>"+
-            "<a target='_blank' href='http://doc.activeeon.com/" + "@docVersion@"  +"/user/ProActiveUserGuide.html#_calendar_service'>" +
-                    "See calendar Documentation and Installation</a> "
-            +"</font>";
+    private void loadWindowsContent(RequestBuilder.Method method) {
 
-    private String getDocumentVersion(){
-        return Config.get().getVersion().contains("SNAPSHOT") ? "latest"
-                : Config.get().getVersion();
+        String host = com.google.gwt.user.client.Window.Location.getHostName();
+        String user = LoginModel.getInstance().getLogin();
+        String requestUrl = "http://" + host + ":" + SchedulerConfig.get().getCalendarServerPort() +
+            "/calendar-service/private-urls/" + user + "/";
+
+        RequestBuilder rb = new RequestBuilder(method, requestUrl);
+        rb.setCallback(new RequestCallback() {
+
+            @Override
+            public void onResponseReceived(Request request, Response response) {
+                pane.clear();
+                root.clear();
+                window.clear();
+
+                if (200 == response.getStatusCode()) {
+                    text.setContents(buildText(response.getText()));
+                } else {
+                    text.setContents("Error : status code " + response.getStatusCode());
+                }
+
+                pane.addMember(img);
+                pane.addMember(text);
+
+                root.addMember(pane);
+                root.addMember(buttons);
+
+                window.addItem(root);
+                window.show();
+            }
+
+            @Override
+            public void onError(Request request, Throwable exception) {
+                pane.clear();
+                root.clear();
+                window.clear();
+
+                text.setContents("Exception : " + exception.getMessage());
+                pane.addMember(img);
+                pane.addMember(text);
+
+                root.addMember(pane);
+                root.addMember(buttons);
+
+                window.addItem(root);
+                window.show();
+            }
+
+        });
+
+        try {
+            rb.send();
+
+        } catch (RequestException e) {
+            com.google.gwt.user.client.Window.alert("error = " + e.getMessage());
+        }
+
     }
 
-    private String fillTemplate(){
-        return CALENDAR_CONTEXT.replace("@hostURL@",  com.google.gwt.user.client.Window.Location.getHostName())
-                .replace("@login@", LoginModel.getInstance().getLogin())
-                .replace("@docVersion@", getDocumentVersion());
+    private String buildText(String responseText) {
+
+        final StringBuilder sb = new StringBuilder(CALENDAR_CONTEXT);
+
+        if (responseText != null && !responseText.equals("")) {
+            final String host = com.google.gwt.user.client.Window.Location.getHostName();
+            final String user = LoginModel.getInstance().getLogin();
+            String privateUrl = "http://" + host + ":5232/" + user + "/" + responseText;
+
+            sb.append(
+                    "<font size=\"3\"> Private Calendar URL without authentication (Outlook, Google Calendar):<br><br><i>" +
+                        privateUrl +
+                        "</i><br><br>Do not share this URL. <b>Regenerate</b> or <b>Delete</b> it if URL is compromised.</font>");
+
+            buttons.addMember(regenerateBt);
+            buttons.addMember(deleteBt);
+            buttons.removeMember(createBt);
+
+        } else {
+            sb.append(
+                    "<font size=\"3\"> Private Calendar URL without authentication (Outlook, Google Calendar): <br><b>Create</b> if needed.</font>");
+
+            buttons.addMember(createBt);
+            buttons.removeMember(regenerateBt);
+            buttons.removeMember(deleteBt);
+        }
+
+        sb.append("<br><br><br><font size=\"3\"><a target='_blank' href='http://doc.activeeon.com/" +
+            getDocumentVersion() +
+            "/user/ProActiveUserGuide.html#_calendar_service'>See calendar Documentation and Installation</a></font> ");
+
+        return sb.toString();
     }
+
 }
