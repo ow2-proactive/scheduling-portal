0bff4fdd9c16a19ec4cb77591cc797f4434178a6
laurianed <lauriane.douais@activeeon.com>
2017-03-10 16:08:57 +0100

    Add table (key;value) for generic information in the Job Info panel

---
+++ scheduler-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/scheduler/client/view/grid/jobs/GenericInformationColumnsFactory.java
@@ -0,0 +1,58 @@
+/*
+ * ProActive Parallel Suite(TM):
+ * The Open Source library for parallel and distributed
+ * Workflows & Scheduling, Orchestration, Cloud Automation
+ * and Big Data Analysis on Enterprise Grids & Clouds.
+ *
+ * Copyright (c) 2007 - 2017 ActiveEon
+ * Contact: contact@activeeon.com
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation: version 3 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ */
+package org.ow2.proactive_grid_cloud_portal.scheduler.client.view.grid.jobs;
+
+import java.util.Map;
+
+import org.ow2.proactive_grid_cloud_portal.scheduler.client.view.grid.ColumnsFactory;
+import org.ow2.proactive_grid_cloud_portal.scheduler.client.view.grid.GridColumns;
+
+import com.smartgwt.client.data.Record;
+
+
+/**
+ * A factory that give columns and records for jobs.
+ */
+public class GenericInformationColumnsFactory implements ColumnsFactory<Map.Entry<String, String>> {
+
+    public static final GridColumns KEY_ATTR = new GridColumns("key", "Key", 120, true, true);
+
+    public static final GridColumns VALUE_ATTR = new GridColumns("value", "Value", 120, true, false);
+
+    private static final GridColumns[] COLUMNS = new GridColumns[] { KEY_ATTR, VALUE_ATTR };
+
+    @Override
+    public GridColumns[] getColumns() {
+        return COLUMNS;
+    }
+
+    @Override
+    public void buildRecord(Map.Entry<String, String> item, Record record) {
+        record.setAttribute(KEY_ATTR.getName(), item.getKey());
+        record.setAttribute(VALUE_ATTR.getName(), item.getValue());
+    }
+
+}
