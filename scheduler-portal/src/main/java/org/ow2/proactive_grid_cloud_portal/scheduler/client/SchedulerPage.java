f2852be892c019fa8581731568c77000fd2201f2
Amine Mouhoub <ma.mouhoub@gmail.com>
2017-11-14 12:08:03 +0100

    remove calendar service button

--- scheduler-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/scheduler/client/SchedulerPage.java
+++ scheduler-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/scheduler/client/SchedulerPage.java
@@ -507,17 +507,6 @@ public class SchedulerPage implements SchedulerStatusListener, LogListener, Exec
         });
         errorButton.hide();
 
-        ToolStripButton calendarButton = new ToolStripButton("Calendar");
-        calendarButton.setIcon(SchedulerImages.instance.calendar().getSafeUri().asString());
-        calendarButton.setIconSize(20);
-        calendarButton.setTooltip("Import Workflows scheduled at a given date & time in your favorite Calendar, Re-schedule Jobs, and Add Periodicity.");
-        calendarButton.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                CalendarInfoWindow w = new CalendarInfoWindow();
-                w.show();
-            }
-        });
-
         ToolStripButton resourceManagerLinkButton = toolButtonsRender.getResourceManagerLinkButton();
         ToolStripButton studioLinkButton = toolButtonsRender.getStudioLinkButton();
         ToolStripButton cloudAutomationLinkButton = toolButtonsRender.getCloudAutomationLinkButton();
@@ -530,7 +519,6 @@ public class SchedulerPage implements SchedulerStatusListener, LogListener, Exec
         tools.addSeparator();
         tools.addButton(submitButton);
         tools.addSeparator();
-        tools.addButton(calendarButton);
         tools.addButton(errorButton);
         tools.addFill();
         tools.addButton(studioLinkButton);
