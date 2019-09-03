6b31ff55a616a599f41457b061bf14e61e8c8ea4
Mykhailenko <hlib.mykhailenko@gmail.com>
2019-09-03 11:35:48 +0200

    Fave lifting

--- rm-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/rm/client/nodesource/NodeSourceWindow.java
+++ rm-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/rm/client/nodesource/NodeSourceWindow.java
@@ -212,7 +212,7 @@ public abstract class NodeSourceWindow {
     protected void buildForm() {
         VLayout nodeSourceWindowLayout = new VLayout();
         nodeSourceWindowLayout.setMargin(5);
-        nodeSourceWindowLayout.setMembersMargin(5);
+        nodeSourceWindowLayout.setMembersMargin(0);
         HLayout nodeSourceWindowSubLayoutTop = new HLayout(5);
         HLayout nodeSourceWindowSubLayoutBottom = new HLayout(5);
 
@@ -220,7 +220,7 @@ public abstract class NodeSourceWindow {
         nodeSourcePluginsLayout.setHeight(26);
         this.generalParametersLabel = new Label("General Parameters:");
         this.generalParametersLabel.setStyleName("generalParametersStyle");
-        this.generalParametersLabel.setHeight("20px");
+        this.generalParametersLabel.setHeight("15px");
         this.generalParametersLabel.setMargin(5);
 
         this.nodeSourcePluginsWaitingLabel = new Label(this.waitingMessage);
@@ -278,7 +278,7 @@ public abstract class NodeSourceWindow {
         createNodeSourceLayout.addMember(formIsAdvanced);
 
         Label importantFieldsLabel = new Label();
-        importantFieldsLabel.setHeight("20px");
+        importantFieldsLabel.setHeight("15px");
         importantFieldsLabel.setContents("<span style='color:#E86D1F'>* Requested or Important Fields</span>");
         createNodeSourceLayout.addMember(importantFieldsLabel);
 
@@ -550,6 +550,7 @@ public abstract class NodeSourceWindow {
         this.formItemsByName.put(HIDDEN_POLICY, Collections.singletonList(new HiddenItem(HIDDEN_POLICY)));
 
         this.nodesRecoverableCheckbox = new CheckboxItem(NODES_RECOVERABLE_FORM_KEY, "Nodes Recoverable");
+        this.nodesRecoverableCheckbox.setHeight("15px");
         this.nodesRecoverableCheckbox.setTooltip("Defines whether the nodes of this node source can be recovered after a crash of the Resource Manager");
         this.formItemsByName.put(NODES_RECOVERABLE_FORM_KEY, Collections.singletonList(this.nodesRecoverableCheckbox));
         this.formItemsByName.put("generalParametersSpacer", Collections.singletonList(new RowSpacerItem()));
