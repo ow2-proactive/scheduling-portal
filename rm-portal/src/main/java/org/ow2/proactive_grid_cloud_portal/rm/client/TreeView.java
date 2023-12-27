d4335d9363bd1df5dd13dc2e64b043e083b9f41c
Alexandre Falessi <alexandre.falessi@activeeon.com>
2023-12-27 17:08:05 -0500

    use tree.openAll()

--- rm-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/rm/client/TreeView.java
+++ rm-portal/src/main/java/org/ow2/proactive_grid_cloud_portal/rm/client/TreeView.java
@@ -416,7 +416,7 @@ public class TreeView implements NodesListener, NodeSelectedListener {
         treeGrid.markForRedraw();
 
         if (isExpandAll) {
-            expandAll();
+            tree.openAll();
         }
     }
 
